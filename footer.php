8a84050663f5db773903ca9ca6b97c5f3bec8664
Brandon <brandon@builtbybillt.com>
2024-07-22 11:09:03 -0500

    header and footer and bootstrap

---
+++ footer.php
@@ -0,0 +1,12 @@
+</main>
+</div>
+<footer id="footer" role="contentinfo">
+    <div id="copyright">
+        &copy;2024 ENT Kids Adults. All Rights Reserved
+    </div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+
+</html>
