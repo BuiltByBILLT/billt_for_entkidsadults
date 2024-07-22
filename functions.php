69cb896c4df4f4f50bee17e174e25e45ee3bbf3e
Brandon <brandon@builtbybillt.com>
2024-07-22 12:35:58 -0500

    seperated blank slate css, added block css, move bootstrap to its own folders, updated enqueueing

--- functions.php
+++ functions.php
@@ -2,15 +2,21 @@
 
 function load_css()
 {
-    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
+    wp_register_style('blankslate', get_template_directory_uri() . '/css//blankslate.css', array(), false, 'all');
+    wp_enqueue_style('blankslate');
+    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap/bootstrap.min.css', array(), false, 'all');
     wp_enqueue_style('bootstrap');
+    wp_register_style('blocks', get_template_directory_uri() . '/css//blocks.css', array(), false, 'all');
+    wp_enqueue_style('blocks');
+    wp_register_style('mystyles', get_template_directory_uri() . '/css/mystyles.css', array(), false, 'all');
+    wp_enqueue_style('mystyles');
 }
 add_action('wp_enqueue_scripts', 'load_css');
 
 function load_js()
 {
     wp_enqueue_script('jquery');
-    wp_register_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', 'jquery', false, true);
+    wp_register_script('bootstrap', get_template_directory_uri() . '/js//bootstrap/bootstrap.min.js', 'jquery', false, true);
     wp_enqueue_script('bootstrap');
 }
 add_action('wp_enqueue_scripts', 'load_js');
