3e77ae85f6d501c6503dba3d3fe4c6cf3ffa3958
Brandon <brandon@builtbybillt.com>
2024-07-22 12:41:51 -0500

    function to change absolute ent urls to relative when in development

--- functions.php
+++ functions.php
@@ -20,3 +20,29 @@ function load_js()
     wp_enqueue_script('bootstrap');
 }
 add_action('wp_enqueue_scripts', 'load_js');
+
+// Changes urls to relative when in development
+function adjust_links_script()
+{
+?>
+    <script type="text/javascript">
+        document.addEventListener("DOMContentLoaded", function() {
+            const hostname = window.location.hostname;
+            const links = document.querySelectorAll('a');
+
+            links.forEach(function(link) {
+                const linkHostname = new URL(link.href).hostname;
+
+                if (hostname !== 'www.entkidsadults.com' && linkHostname === 'www.entkidsadults.com') {
+                    // Convert to relative URL
+                    link.href = link.href.replace('https://www.entkidsadults.com', '');
+                } else if (hostname === 'www.entkidsadults.com' && linkHostname !== 'www.entkidsadults.com') {
+                    // Ensure it remains an absolute URL
+                    link.href = 'https://www.entkidsadults.com' + link.pathname;
+                }
+            });
+        });
+    </script>
+<?php
+}
+add_action('wp_footer', 'adjust_links_script');
