3e1abf7233faa5202e4ee2ec1b48a193a9454933
Brandon <brandon@builtbybillt.com>
2024-08-07 19:19:05 -0500

    removed bootstrap

--- functions.php
+++ functions.php
@@ -5,9 +5,6 @@ function load_css()
 {
     wp_enqueue_style('poppins-font', 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap', false);
 
-    // wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap/bootstrap.min.css', array(), false, 'all');
-    // wp_enqueue_style('bootstrap');
-
     wp_register_style('blocks', get_template_directory_uri() . '/css//blocks.css', array(), false, 'all');
     wp_enqueue_style('blocks');
 
@@ -22,10 +19,6 @@ add_action('wp_enqueue_scripts', 'load_css');
 // Load scripts
 function load_js()
 {
-    wp_enqueue_script('jquery');
-    wp_register_script('bootstrap', get_template_directory_uri() . '/js//bootstrap/bootstrap.min.js', 'jquery', false, true);
-    wp_enqueue_script('bootstrap');
-
     // Changes urls to relative when in development
     wp_register_script('adjust-links', get_template_directory_uri() . '/js/adjust-links.js', array(), false, true);
     wp_enqueue_script('adjust-links');
