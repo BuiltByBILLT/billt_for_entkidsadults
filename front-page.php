3d7336c34059e8b429a76c1a4a1a6f5fbd07b78f
Brandon <brandon@builtbybillt.com>
2024-08-13 17:20:32 -0500

    hero image and columns

--- front-page.php
+++ front-page.php
@@ -1,17 +1,22 @@
 <?php get_header(); ?>
 <main id="content" role="main">
     <div class='hero'>
-        <h1>Front Page</h1>
-        <p>This is the front page</p>
-        <p>This is the front page</p>
-        <p>This is the front page</p>
-        <p>This is the front page</p>
-        <p>This is the front page</p>
-        <p>This is the front page</p>
-        <p>This is the front page</p>
-        <p>This is the front page</p>
-        <p>This is the front page</p>
-        <p>This is the front page</p>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>Expert Care For Your Ear, Nose & Throat Health</h1>
+                    <p>Our team of board-certified physicians and audiologists are dedicated to providing the highest quality care for all of your ear, nose, and throat needs.</p>
+                    <div class="row">
+                        <button>Schedule appointment</button>
+                        <button>Our Services</button>
+                    </div>
+                </div>
+                <div class="col">
+                </div>
+            </div>
+        </div>
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Dr-Gambe-Dr-Kubala.png" alt="Dr-Gambe-Dr-Kubala">
+
     </div>
 
     <div id="container" class="container">
