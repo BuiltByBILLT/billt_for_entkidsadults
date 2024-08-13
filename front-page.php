7f37e9d1c063ae010a5efb1aa1003f0294dcfc75
Brandon <brandon@builtbybillt.com>
2024-08-13 18:53:45 -0500

    all hero col elements

--- front-page.php
+++ front-page.php
@@ -3,12 +3,19 @@
     <div class='hero'>
         <div class="container">
             <div class="row">
-                <div class="col">
-                    <h1>Expert Care For Your Ear, Nose & Throat Health</h1>
-                    <p>Our team of board-certified physicians and audiologists are dedicated to providing the highest quality care for all of your ear, nose, and throat needs.</p>
-                    <div class="row">
-                        <button>Schedule appointment</button>
-                        <button>Our Services</button>
+                <div class="col left-col">
+                    <h1 class="color-dark-blue">Expert Care For Your <span class="color-light-blue">Ear</span>, <span class="color-light-blue">Nose</span> & <span class="color-light-blue">Throat</span> Health</h1>
+                    <div class="text">
+                        <p>Otolaryngology Specialists of North Texas are committed to providing outstanding service to our patients and referring physicians.</p>
+                        <p>Our practice goal is to improve the health and well-being of our patients and the communities we serve.</p>
+                    </div>
+                    <div class="row button-row">
+                        <button class="primary-button">Schedule appointment</button>
+                        <button class="outline-button">Our Services</button>
+                    </div>
+                    <div class="row icon-row">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/voted-best-d.png" alt="voted-best-d">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/plano-moms.png" alt="plano-moms">
                     </div>
                 </div>
                 <div class="col">
