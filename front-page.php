1f282791d16e7bc3dc4ca807b03ce8331e93cbfa
Brandon <brandon@builtbybillt.com>
2024-08-13 20:27:53 -0500

    modularized front page php and css, added learn more section

--- front-page.php
+++ front-page.php
@@ -1,35 +1,30 @@
 <?php get_header(); ?>
 <main id="content" role="main">
-    <div class='hero'>
-        <div class="container">
-            <div class="row">
-                <div class="col left-col">
-                    <h1 class="color-dark-blue">Expert Care For Your <span class="color-light-blue">Ear</span>, <span class="color-light-blue">Nose</span> & <span class="color-light-blue">Throat</span> Health</h1>
-                    <div class="text">
-                        <p>Otolaryngology Specialists of North Texas are committed to providing outstanding service to our patients and referring physicians.</p>
-                        <p>Our practice goal is to improve the health and well-being of our patients and the communities we serve.</p>
-                    </div>
-                    <div class="row button-row">
-                        <button class="primary-button">Schedule appointment</button>
-                        <button class="outline-button">Our Services</button>
-                    </div>
-                    <div class="row icon-row">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/voted-best-d.png" alt="voted-best-d">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/plano-moms.png" alt="plano-moms">
-                    </div>
-                </div>
-                <div class="col">
-                </div>
-            </div>
-        </div>
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Dr-Gambe-Dr-Kubala.png" alt="Dr-Gambe-Dr-Kubala">
-
-    </div>
+    <?php
+    include(get_template_directory() . '/template-parts/hero-section.php');
+    ?>
 
     <div id="container" class="container">
+        <?php
+        include(get_template_directory() . '/template-parts/learn-more-section.php');
+        ?>
 
-        <?php get_template_part('includes/section', 'content'); ?>
 
+        <section class="about-us-cards">
+
+        </section>
+        <section class="announcements">
+
+        </section>
+        <section class="our-team-cards">
+
+        </section>
+        <section class="book-now">
+
+        </section>
+        <section class="our-offices">
+
+        </section>
     </div>
 </main>
 <?php get_footer(); ?>
---
+++ template-parts/hero-section.php
@@ -0,0 +1,24 @@
+<section class='hero'>
+    <div class="container">
+        <div class="row">
+            <div class="col left-col">
+                <h1 class="color-dark-blue">Expert Care For Your <span class="color-light-blue">Ear</span>, <span class="color-light-blue">Nose</span> & <span class="color-light-blue">Throat</span> Health</h1>
+                <div class="text">
+                    <p>Otolaryngology Specialists of North Texas are committed to providing outstanding service to our patients and referring physicians.</p>
+                    <p>Our practice goal is to improve the health and well-being of our patients and the communities we serve.</p>
+                </div>
+                <div class="row button-row">
+                    <button class="primary-button">Schedule appointment</button>
+                    <button class="outline-button">Our Services</button>
+                </div>
+                <div class="row icon-row">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/voted-best-d.png" alt="voted-best-d">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/plano-moms.png" alt="plano-moms">
+                </div>
+            </div>
+            <div class="col">
+            </div>
+        </div>
+    </div>
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Dr-Gambe-Dr-Kubala.png" alt="Dr-Gambe-Dr-Kubala">
+</section>
