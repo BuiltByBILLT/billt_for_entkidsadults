3e30e185114ec3b9fd36110e4496af2531abd377
Brandon <brandon@builtbybillt.com>
2024-08-25 08:43:27 -0500

    added formspree to contact form

--- template-parts/book-now-section.php
+++ template-parts/book-now-section.php
@@ -3,7 +3,7 @@
         <h5>Schedule Now</h5>
         <h2>Book An Appointment</h2>
         <p>Schedule your appointment by completing the form below. We’ll confirm your visit shortly.</p>
-        <form action="/book-appointment" method="POST">
+        <form action="https://formspree.io/f/xyzglagp" method="POST">
             <input type="text" name="full_name" placeholder="Full Name" required>
             <input type="text" name="phone_number" placeholder="Phone Number" required>
             <input type="email" name="email_address" placeholder="Email Address" required>
