f169ea34db68b556c0788e6cf315a6b8c5e9bc9c
Brandon <brandon@builtbybillt.com>
2024-08-27 16:01:45 -0500

    added home/blog styles

--- home.php
+++ home.php
@@ -1,19 +1,25 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()) : ?>
+<main id="content" role="main" class="container">
+    <h1>Announcements</h1>
+    <div class="blog-list">
+        <?php if (have_posts()) : ?>
 
-    <?php while (have_posts()) : the_post(); ?>
-        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <div class="entry-content">
-                <?php the_excerpt(); ?>
-            </div>
-        </div>
-    <?php endwhile; ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <div class="entry-content">
+                        <?php the_excerpt(); ?>
+                    </div>
+                </div>
+            <?php endwhile; ?>
 
-    <?php the_posts_navigation(); ?>
-<?php else : ?>
-    <p><?php esc_html_e('No posts found.', 'your-theme-textdomain'); ?></p>
-<?php endif; ?>
+            <?php the_posts_navigation(); ?>
+        <?php else : ?>
+            <p><?php esc_html_e('No posts found.', 'your-theme-textdomain'); ?></p>
+        <?php endif; ?>
+
+    </div>
+</main>
 
 <?php get_footer(); ?>
