6c9b79256491e06c05dd79e483442dc615887078
Brandon <brandon@builtbybillt.com>
2024-08-27 15:32:42 -0500

    single page styles

--- single.php
+++ single.php
@@ -1,13 +1,16 @@
 <?php get_header(); ?>
-
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <h1><?php the_title(); ?></h1>
-        <div class="post-content">
-            <?php the_content(); ?>
-        </div>
-    <?php endwhile;
-else : ?>
-    <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
-<?php endif; ?>
+<main id="content" role="main">
+    <div class="container single-page">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <h1><?php the_title(); ?></h1>
+                <div class="post-content">
+                    <?php the_content(); ?>
+                </div>
+            <?php endwhile;
+        else : ?>
+            <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
+        <?php endif; ?>
+    </div>
+</main>
 
 <?php get_footer(); ?>
