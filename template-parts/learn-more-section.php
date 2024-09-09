e7de0957af94f1b5e1902b03258741321ab5168d
Brandon <brandon@builtbybillt.com>
2024-09-09 10:25:33 -0500

    response learn more section on the front page

--- template-parts/learn-more-section.php
+++ template-parts/learn-more-section.php
@@ -1,36 +1,33 @@
 <section class="learn-more-section">
     <div class="row">
-        <div class="col learn-more-images">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/stock-1.png" alt="Doctor examining patient" class="img-top">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/stock-2.png" alt="Doctor with headlamp" class="img-bottom">
-        </div>
+
         <div class="col learn-more-content">
             <h5>Learn More About</h5>
             <h2>Otolaryngology Specialists of North Texas</h2>
             <p>
-                Dr. Greg Rohn, Dr. Brad Gamble, and Dr. Michael Kubala of Otolaryngology Specialists of North Texas (OSNT),
+                Dr. Brad Gamble, and Dr. Michael Kubala of Otolaryngology Specialists of North Texas (OSNT),
                 practice ear, nose, throat, and sinus medical and surgical care for children and adults. Dr. Rohn and Dr. Gamble
                 are board certified Otolaryngology (ENT)-Head and Neck surgeons with offices in Plano and Dallas, Texas.
             </p>
             <div class="learn-more-goals">
-                <div class="goal">
-                    <h3><img src="<?php echo get_template_directory_uri(); ?>/assets/images/check-icon.png" alt="Checkmark" class="check-icon">Our Goal</h3>
-                    <p>
-                        OSNT will provide the highest quality ear, nose, and throat, medical and surgical care to both children and adults.
-                        We are committed to outstanding service to our patients and referring physicians. Our practice goal is to improve the
-                        health and well-being of our patients and the communities we serve.
-                    </p>
-                </div>
-                <div class="separator"></div>
                 <div class="mission">
                     <h3><img src="<?php echo get_template_directory_uri(); ?>/assets/images/check-icon.png" alt="Checkmark" class="check-icon">Our Mission</h3>
                     <p>
                         OSNT will provide the highest quality ear, nose, and throat, medical and surgical care to both children and adults.
-                        We are committed to outstanding service to our patients and referring physicians. Our practice goal is to improve the
-                        health and well-being of our patients and the communities we serve.
+                    </p>
+                </div>
+                <div class="separator"></div>
+                <div class="goal">
+                    <h3><img src="<?php echo get_template_directory_uri(); ?>/assets/images/check-icon.png" alt="Checkmark" class="check-icon">Our Goal</h3>
+                    <p>
+                        Our practice goal is to improve the health and well-being of our patients and the communities we serve.
                     </p>
                 </div>
             </div>
+        </div>
+        <div class="col learn-more-images">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/stock-1.png" alt="Doctor examining patient" class="img-top">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/stock-2.png" alt="Doctor with headlamp" class="img-bottom">
         </div>
     </div>
 </section>
