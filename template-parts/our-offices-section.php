7b3e40a63533b25b39c1e46764ae424fa3df4c4f
Brandon <brandon@builtbybillt.com>
2024-10-14 15:02:26 -0500

    removed book now sections

--- template-parts/our-offices-section.php
+++ template-parts/our-offices-section.php
@@ -1,45 +1,47 @@
 <div class="our-offices-section">
     <h5>Contact Us</h5>
     <h2>Our Offices</h2>
+    <div class="offices-row">
 
-    <div class="office">
-        <a target="_blank" href="https://maps.app.goo.gl/hayWXkhLw9UsAfDz9" class="map">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/map-plano.png" alt="Map of Plano Office">
-        </a>
-        <div class="details">
-            <h4><a href="/contact-us/plano-office/">Plano Office</a></h4>
-            <div class="row office-icon-row phone">
-                <span class="icon"></span>
-                <a target="_blank" href="tel:+19723780633">+1 972-378-0633</a>
-            </div>
-            <div class="row office-icon-row email">
-                <span class="icon"></span>
-                <a target="_blank" href="mailto:mariac@entkidsadults.com">mariac@entkidsadults.com</a>
-            </div>
-            <div class="row office-icon-row address">
-                <span class="icon"></span>
-                <a target="_blank" href="https://maps.app.goo.gl/hayWXkhLw9UsAfDz9">6300 W Parker Rd, Suite G24, <br>Plano, TX 75093</a>
+        <div class="office">
+            <a target="_blank" href="https://maps.app.goo.gl/hayWXkhLw9UsAfDz9" class="map">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/map-plano.png" alt="Map of Plano Office">
+            </a>
+            <div class="details">
+                <h4><a href="/contact-us/plano-office/">Plano Office</a></h4>
+                <div class="row office-icon-row phone">
+                    <span class="icon"></span>
+                    <a target="_blank" href="tel:+19723780633">+1 972-378-0633</a>
+                </div>
+                <div class="row office-icon-row email">
+                    <span class="icon"></span>
+                    <a target="_blank" href="mailto:mariac@entkidsadults.com">mariac@entkidsadults.com</a>
+                </div>
+                <div class="row office-icon-row address">
+                    <span class="icon"></span>
+                    <a target="_blank" href="https://maps.app.goo.gl/hayWXkhLw9UsAfDz9">6300 W Parker Rd, Suite G24, <br>Plano, TX 75093</a>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="office">
-        <a target="_blank" href="https://maps.app.goo.gl/nYAczHK7GM6B6HBG7" class=" map">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/map-dallas.png" alt="Map of Dallas Office">
-        </a>
-        <div class="details">
-            <h4><a href="/contact-us/dallas-office">Dallas Office</a></h4>
-            <div class="row office-icon-row phone">
-                <span class="icon"></span>
-                <a target="_blank" href="tel:+12142391641">+1 214-239-1641</a>
-            </div>
-            <div class="row office-icon-row email">
-                <span class="icon"></span>
-                <a target="_blank" href="mailto:mariac@entkidsadults.com">mariac@entkidsadults.com</a>
-            </div>
-            <div class="row office-icon-row address">
-                <span class="icon"></span>
-                <a target="_blank" href="https://maps.app.goo.gl/nYAczHK7GM6B6HBG7">7515 Greenville Ave, Suite 410, <br>Dallas, TX, 75231</a>
+        <div class="office">
+            <a target="_blank" href="https://maps.app.goo.gl/nYAczHK7GM6B6HBG7" class=" map">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/map-dallas.png" alt="Map of Dallas Office">
+            </a>
+            <div class="details">
+                <h4><a href="/contact-us/dallas-office">Dallas Office</a></h4>
+                <div class="row office-icon-row phone">
+                    <span class="icon"></span>
+                    <a target="_blank" href="tel:+12142391641">+1 214-239-1641</a>
+                </div>
+                <div class="row office-icon-row email">
+                    <span class="icon"></span>
+                    <a target="_blank" href="mailto:mariac@entkidsadults.com">mariac@entkidsadults.com</a>
+                </div>
+                <div class="row office-icon-row address">
+                    <span class="icon"></span>
+                    <a target="_blank" href="https://maps.app.goo.gl/nYAczHK7GM6B6HBG7">7515 Greenville Ave, Suite 410, <br>Dallas, TX, 75231</a>
+                </div>
             </div>
         </div>
     </div>
